ced2f080b66de66f5f035cab3e7a085c977b7ec1
thanks299 <thanksagbeble@gmail.com>
2024-05-16 12:29:47 -0700

    fix my code

---
+++ 0x01-challenge/README.md
@@ -0,0 +1,1 @@
+this is my 0x01-challenge fix my code challenge readme
---
+++ 0x01-challenge/square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square.
+"""
+
+class Square:
+	"""
+	A class used to represent a Square
+	"""
+
+	width = 0
+	height = 0
+
+	def __init__(self, *args, **kwargs):
+		"""
+		Initialize the square with optional width and height
+
+		Parameters:
+		*args (tuple): Additional positional arguments (not used)
+		**kwargs (dict): Key-value pairs for width and height
+		"""
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+	def area_of_my_square(self):
+		"""
+		Calculate the area of the square
+
+		Returns:
+		int: The area of the square
+		"""
+		return self.width * self.width
+
+	def PerimeterOfMySquare(self):
+		"""
+		Calculate the perimeter of the square
+
+		Returns:
+		int: The perimeter of the square
+		"""
+		return 2 * (self.width + self.height)
+
+	def __str__(self):
+		"""
+		Provide a string representation of the square
+
+		Returns:
+		str: The width and height of the square separated by a '/'
+		"""
+		return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+	"""
+	Main entry point of the module
+	"""
+	s = Square(width=12, height=9)
+	print(s)
+	print(s.area_of_my_square())
+	print(s.PerimeterOfMySquare())
